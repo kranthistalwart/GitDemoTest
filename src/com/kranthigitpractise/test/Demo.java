40224c4e2996253a6ef51dd4dbc43aad773cdb01
kranthistalwart <kranthistalwart143>
2019-10-20 18:49:04 +0530

    Made second changes

--- src/com/kranthigitpractise/test/Demo.java
+++ src/com/kranthigitpractise/test/Demo.java
@@ -3,7 +3,9 @@ package com.kranthigitpractise.test;
 public class Demo 
 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println("Created second new class");
+		System.out.println("Added second edit");
 	}
 }
